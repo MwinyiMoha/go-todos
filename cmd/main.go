1763f30be168a73ee4846e906aef4acd98617f79
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2022-03-19 15:03:49 +0300

    Bring it all together

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"go-todos/internal/core/services"
+	"go-todos/internal/framework/api"
+	"go-todos/internal/framework/store"
+	"log"
+)
+
+func main() {
+	repository := store.NewStore()
+	service := services.NewTodoService(repository)
+	controller := api.NewHTTPController(service)
+	router := api.NewRouter(controller)
+	router.AddRoutes()
+
+	log.Fatal(router.Serve())
+}
