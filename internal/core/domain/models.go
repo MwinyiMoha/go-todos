4337bcaa139cd033b3a4385b86c6f143d92350f0
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2025-11-21 14:20:36 +0300

    Update Todo model

--- internal/core/domain/models.go
+++ internal/core/domain/models.go
@@ -6,20 +6,18 @@ import (
 	"github.com/google/uuid"
 )
 
-type TodoPayload struct {
-	Description string `json:"description"`
-}
-
 type Todo struct {
 	ID          string    `json:"id" bson:"id"`
 	Description string    `json:"description" bson:"description"`
 	CreatedAt   time.Time `json:"created_at" bson:"createdAt"`
+	UpdatedAt   time.Time `json:"updated_at" bson:"updatedAt"`
 }
 
-func NewTodo(desc string) *Todo {
+func NewTodo(description string) *Todo {
 	return &Todo{
 		ID:          uuid.New().String(),
-		Description: desc,
+		Description: description,
 		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
 	}
 }
