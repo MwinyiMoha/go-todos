71ee9f503427b2546dabe3c56ac82d6debef971f
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2025-11-21 14:21:41 +0300

    Update repository interface

--- internal/core/ports/app_repository.go
+++ internal/core/ports/app_repository.go
@@ -2,8 +2,8 @@ package ports
 
 import "go-todos/internal/core/domain"
 
-type TodoRepository interface {
-	ListTodos() (*[]domain.Todo, error)
+type AppRepository interface {
+	ListTodos() ([]*domain.Todo, error)
 	RetrieveTodo(id string) (*domain.Todo, error)
 	AddTodo(description string) (*domain.Todo, error)
 	EditTodo(id string, description string) (*domain.Todo, error)
