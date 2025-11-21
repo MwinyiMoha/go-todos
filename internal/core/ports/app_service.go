97ef4f1b95745118e22e27b0a0d463d04ffbff59
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2025-11-21 14:56:31 +0300

    Update the service interface

--- internal/core/ports/app_service.go
+++ internal/core/ports/app_service.go
@@ -2,8 +2,8 @@ package ports
 
 import "go-todos/internal/core/domain"
 
-type TodoService interface {
-	GetTodos() (*[]domain.Todo, error)
+type AppService interface {
+	GetTodos() ([]*domain.Todo, error)
 	GetTodo(id string) (*domain.Todo, error)
 	CreateTodo(description string) (*domain.Todo, error)
 	UpdateTodo(id string, description string) (*domain.Todo, error)
